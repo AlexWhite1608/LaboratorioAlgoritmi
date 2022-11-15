f8138ba256f3984358eb2d3d38c72fa2d8e4afab
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2022-11-15 13:50:13 +0100

    funziona aggiunta nodi/archi

--- src/CC and MST/main.py
+++ src/CC and MST/main.py
@@ -7,10 +7,13 @@ def main():
     adj_matrix = g.random_adj_matrix(4)
     graph = g.Graph(adj_matrix)
 
+    # Aggiungo un nodo ed un arco random
+    graph.add_node("X")
+    graph.add_edge(("X", "A"))
+
     print(graph)
 
     graph_visual = gv.GraphVisual(graph.get_graph_dict())
-
     graph_visual.visualize()
 
 
